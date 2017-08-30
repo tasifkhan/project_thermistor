6492821fcb6ddaf36d5873421b35a95064dbb0cc
Yasser Khan <tasif.khan@gmail.com>
2017-08-29 18:01:01 -0700

    hardware and firmware are working

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # project_thermistor
-Stream, visualize, and save 16 channel thermistor data using arduino due.
-
-## Note
-'Thermistor array is connected to the 32 pin .5 mm pitch FFC connector.
+- Stream, visualize, and save 16 channel thermistor data using arduino due.
+- Thermistor array is connected to the 32 pin .5 mm pitch FFC connector.
 Those connections goes to the analog switch, and to the bridge circuit.
-Switch address connections
+- Switch address connections
+| MUX Pins| Arduino Pins |
+| :------:|:------------:|
+| EN      |53|
+| A0      |51|
+| A1      |49|
+| A2      |47|
+| A3      |45|
 
-EN 53
-A0 51
-A1 49
-A2 47
-A3 45'
+## notes
+- 2017_08_27 checked both hardware and firmware, correctly outputting resistance values. 
--- firmware/arduino_resistance_stream/arduino_resistance_stream.ino
+++ firmware/arduino_resistance_stream/arduino_resistance_stream.ino
@@ -1,30 +1,34 @@
 // stream data from 16 channels 
+// time pixel id resistance0 ref_resistance [MOhm]
+// 2017_08_29 working version
 
 unsigned long globalTime;
 byte controlPins[] = {
-  //A3 A2 A1 A0 EN On Switch
-  //X1 X1 X1 X1 0 None
-  B00001000,
-  B00011000,
-  B00101000,
-  B00111000,
-  B01001000,
-  B01011000,
-  B01101000,
-  B01111000,
-  B10001000,
-  B10011000,
-  B10101000, 
-  B10111000, 
-  B11001000, 
-  B11011000, 
-  B11101000, 
-  B11111000  }; 
+  //A3 A2 A1 A0 EN 
+  //45 47 49 51 53
+  B00000001,
+  B00000011,
+  B00000101,
+  B00000111,
+  B00001001,
+  B00001011,
+  B00001101,
+  B00001111,
+  B00010001,
+  B00010011,
+  B00010101, 
+  B00010111, 
+  B00011001, 
+  B00011011, 
+  B00011101, 
+  B00011111  }; 
  
-// store adc values here                 
-float muxValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
-float muxValues1[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
- 
+// store adc values here   
+float v0[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // voltage reading 
+float v1[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // voltage reading               
+float muxValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // resistance 
+float muxValues1[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // resistance
+float dummy[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
 void setup()
 {
   Serial.begin(9600);
@@ -33,6 +37,8 @@ void setup()
   pinMode(49, OUTPUT); // A1
   pinMode(51, OUTPUT); // A0
   pinMode(53, OUTPUT); // EN
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
   
 }
  
@@ -46,9 +52,9 @@ void displayData()
     Serial.print(' ');
     Serial.print(i); // pixelID 
     Serial.print(' '); 
-    Serial.print(muxValues[i]); // adc value
+    Serial.print(muxValues[i]); // resistance
     Serial.print(' '); 
-    Serial.println(muxValues1[i]); // adc value
+    Serial.println(muxValues1[i]); // resistance
   }
   //Serial.println("========================");  
 }
@@ -57,27 +63,40 @@ void loop()
 {
   globalTime = int(millis() / 1000);
   
-  for (int i = 0; i < 16; i++)
+  for (int i = 0; i < 16; i++) // reading one channel at a time
   {
-    // shift by 3 bit and writes the last bit to the pin
-    digitalWrite(53, controlPins[i]>>3 & B00000001);
+    // shift by 1 bit and writes the last bit to the pin
+    digitalWrite(53, controlPins[i]>>0 & B00000001); //EN
+    //Serial.print(controlPins[i]>>0 & B00000001);
+    digitalWrite(51, controlPins[i]>>1 & B00000001); //A0
+    //Serial.print(controlPins[i]>>1 & B00000001);
+    digitalWrite(49, controlPins[i]>>2 & B00000001); //A1
+    //Serial.print(controlPins[i]>>2 & B00000001);
+    digitalWrite(47, controlPins[i]>>3 & B00000001); //A2
     //Serial.print(controlPins[i]>>3 & B00000001);
-    digitalWrite(51, controlPins[i]>>4 & B00000001);
-    //Serial.print(controlPins[i]>>4 & B00000001);
-    digitalWrite(49, controlPins[i]>>5 & B00000001);
-    //Serial.print(controlPins[i]>>5 & B00000001);
-    digitalWrite(47, controlPins[i]>>6 & B00000001);
-    //Serial.print(controlPins[i]>>6 & B00000001);
-    digitalWrite(46, controlPins[i]>>7 & B00000001);
-    //Serial.println(controlPins[i]>>7 & B00000001);
-    
-    muxValues[i]=3.3*(analogRead(0)/1024.0); // read the vlaue on that pin and store in array
-    muxValues1[i]=3.3*(analogRead(1)/1024.0); // read the vlaue on that pin and store in array
+    digitalWrite(45, controlPins[i]>>4 & B00000001); //A3
+    //Serial.println(controlPins[i]>>4 & B00000001);
 
-    delay(100);
+    // dummy reads otherwise the adc goes all over the places - v0
+    for (int j = 0; j < 5; j++){
+      dummy[j]= analogRead(A0);
+      delay(10);
+    }
+    v0[i]=3.3*(analogRead(A0)/1024.0); // v0
+    muxValues[i]=((.964*v0[i])/(3.3)) / (1-(v0[i]/3.3)); // (r0*(v0/3.3))/(1-(v0/3.3)) = rx
+    delay(50);
+
+    // dummy reads otherwise the adc goes all over the places - v1
+    for (int j = 0; j < 5; j++){
+      dummy[j]=analogRead(A1);
+      delay(10);
+    }
+    v1[i]=3.3*(analogRead(A1)/1024.0); // read the vlaue on that pin and store in array
+    muxValues1[i]=((.985*v1[i])/(3.3)) / (1-(v1[i]/3.3));
+    delay(50);
   }
  
   // display captured data
   displayData();
-  delay(2000); 
+  delay(1000); 
 }
